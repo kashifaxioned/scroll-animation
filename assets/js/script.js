f1079d3e38c46c20be9f09fdb1b9ab825c02b874
kashifaxioned <kashifa@axioned.com>
2022-04-08 11:20:04 +0530

    JS functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,17 @@
 
 */
 
+let boxes = document.querySelectorAll(".box")
 
+let obersrver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("reveal", entry.isIntersecting);
+    })
+}, { threshold: .5 })
 
-
-
-
-
-
-
-
-
-
+boxes.forEach(box => {
+    obersrver.observe(box)
+})
 
 
 
